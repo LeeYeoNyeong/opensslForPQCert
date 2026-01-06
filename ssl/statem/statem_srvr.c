73f843afb62860d88274c4e5f6b3a233178082b1
LeeYeoNyeong <yn4103@naver.com>
2026-01-06 16:35:51 +0900

    time check

--- ssl/statem/statem_srvr.c
+++ ssl/statem/statem_srvr.c
@@ -2316,6 +2316,8 @@ int tls_handle_alpn(SSL_CONNECTION *s)
 
 WORK_STATE tls_post_process_client_hello(SSL_CONNECTION *s, WORK_STATE wst)
 {
+    OSSL_TIME start = ossl_time_now();
+
     const SSL_CIPHER *cipher;
     SSL *ssl = SSL_CONNECTION_GET_SSL(s);
     SSL *ussl = SSL_CONNECTION_GET_USER_SSL(s);
@@ -2429,6 +2431,12 @@ WORK_STATE tls_post_process_client_hello(SSL_CONNECTION *s, WORK_STATE wst)
         }
     }
 #endif
+
+    OSSL_TIME end = ossl_time_now();
+    uint64_t elapsed = ossl_time2us(ossl_time_subtract(end, start));
+    printf("\n=========================================================\n\n");
+    fprintf(stderr, "[TIMING] Post-process ClientHello: %lu us\n", (unsigned long)elapsed);
+    printf("\n=========================================================\n");
 
     return WORK_FINISHED_STOP;
  err:
